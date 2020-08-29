d3c58519e654916c3d2a8d84ef6366ad0400c204
Sven Wiegand <sven.wiegand@orgavision.com>
2020-08-29 11:13:38 +0200

    Support all avatar variants

--- code/dataDisplay/Avatar.tsx
+++ code/dataDisplay/Avatar.tsx
@@ -2,6 +2,7 @@ import * as React from "react"
 import { addPropertyControls, ControlType } from "framer"
 import { withTheme } from "../common/theme"
 import { Avatar as MuiAvatar, Icon } from '@material-ui/core'
+import { propertyControls } from "../common/propertyControl"
 
 export function Avatar(props) {
     const { letters, icon, color, size, width, height, ...avatarProps } = props
@@ -17,32 +18,39 @@ export function Avatar(props) {
     )
 }
 
-addPropertyControls(Avatar, {
-    src: {
+addPropertyControls(Avatar, propertyControls(
+    ["src", {
         type: ControlType.Image,
         title: "Image",
         defaultValue: "https://images.unsplash.com/photo-1537815749002-de6a533c64db?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
-    },
-    letters: {
+    }],
+    ["letters", {
         type: ControlType.String,
         title: "Letters",
-        defaultValue: "MD"
-    },
-    icon: {
+        defaultValue: ""
+    }],
+    ["icon", {
         type: ControlType.String,
         title: "Icon",
         placeholder: "Icon name",
-        defaultValue: ""
-    },
-    color: {
+        defaultValue: "person"
+    }],
+    ["variant", {
+        type: ControlType.Enum,
+        title: "Variant",
+        options: ["circle", "rounded", "square"],
+        defaultValue: "circle"
+    }],
+    ["color", {
         type: ControlType.Color,
         title: "Fallback color",
         defaultValue: "#b5b5b5"
-    },
-    size: {
+    }],
+    ["size", {
         type: ControlType.Enum,
         title: "Size",
         options: ["native", "fill"],
         defaultValue: "native"
-    }
-})
+    }],
+    "onClick"
+))
