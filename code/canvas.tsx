93fa8a20f95a1a34c681185691c13e72acea835c
Sven Wiegand <sven.wiegand@orgavision.com>
2020-09-08 14:05:12 +0200

    Full color definition

--- code/canvas.tsx
+++ code/canvas.tsx
@@ -41,4 +41,26 @@ export const colors = Object.freeze({
     "error.main": "var(--token-0ded97c6-18cd-47ac-bed7-05fabd1c2856, rgb(255, 71, 71))",
     /** #FF3333 */
     "error.dark": "var(--token-7518fcb0-73d3-429d-aec3-8e69bbe0cd18, rgb(255, 51, 51))",
+    /** rgba(0, 0, 0, 0.87) */
+    "text.primary": "var(--token-49dad9a0-482f-4193-8d4e-72e0699e757d, rgba(0, 0, 0, 0.87))",
+    /** rgba(0, 0, 0, 0.54) */
+    "text.secondary": "var(--token-38ad3752-125d-479b-9f46-05f870551e81, rgba(0, 0, 0, 0.54))",
+    /** rgba(0, 0, 0, 0.38) */
+    "text.disabled": "var(--token-6ba764f4-0c2b-4db8-a130-c56aad675896, rgba(0, 0, 0, 0.38))",
+    /** rgba(0, 0, 0, 0.54) */
+    "action.active": "var(--token-591a5859-9eaa-44e2-ac4c-83717a2a2ee6, rgba(0, 0, 0, 0.54))",
+    /** rgba(0, 0, 0, 0.04) */
+    "action.hover": "var(--token-ef327786-13ab-41e2-84ff-b9a30fd63616, rgba(0, 0, 0, 0.04))",
+    /** rgba(0, 0, 0, 0.08) */
+    "action.selected": "var(--token-dbc1a591-80d3-4d23-bf28-b882d2f31f63, rgba(0, 0, 0, 0.08))",
+    /** rgba(0, 0, 0, 0.26) */
+    "action.disabled": "var(--token-e5b11104-faf1-43ad-ba30-83c8ea214a9f, rgba(0, 0, 0, 0.26))",
+    /** rgba(0, 0, 0, 0.12) */
+    "action.disabledBackground": "var(--token-975efc3e-c233-4905-bdd7-fe37491fe1e9, rgba(0, 0, 0, 0.12))",
+    /** #FAFAFA */
+    "background.default": "var(--token-4d927322-2b9a-47c5-aab4-a15e532e9cc7, rgb(250, 250, 250))",
+    /** #FFFFFF */
+    "background.paper": "var(--token-92dd06a1-e3b1-4e62-a46d-46f5ee9ababb, rgb(255, 255, 255))",
+    /** rgba(0, 0, 0, 0.12) */
+    "divider": "var(--token-69c6e079-d883-406c-9b09-c4797627c8ea, rgba(0, 0, 0, 0.12))",
 })
