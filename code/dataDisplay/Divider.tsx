01e7e8ae9495244cb1e7721c244e4e76753a225c
Sven Wiegand <sven.wiegand@orgavision.com>
2020-12-10 16:24:54 +0100

    Introduced divider

---
+++ code/dataDisplay/Divider.tsx
@@ -0,0 +1,25 @@
+import { Divider as MuiDivider } from "@material-ui/core"
+import { addPropertyControls, FrameProps } from "framer"
+import * as React from "react"
+import { Control } from "../common/propertyControl"
+import { withTheme } from "../common/theme"
+
+type Props = Partial<FrameProps> & {
+    flexItem: boolean
+    light: boolean,
+    orientation: "horizontal" | "vertical"
+    variant: "fullWidth" | "inset" | "middle"
+}
+export function Divider(props: Props) {
+    const { flexItem, light, orientation, variant } = props
+    return withTheme(<MuiDivider flexItem={flexItem} light={light} orientation={orientation} variant={variant} />)
+}
+
+Divider.defaultProps = { width: "100%", height: 1 }
+
+addPropertyControls(Divider, {
+    light: Control.Boolean("Light", false),
+    orientation: Control.Enum("Orientation", ["horizontal", "vertical"], "horizontal"),
+    variant: Control.Enum("Variant", ["fullWidth", "inset", "middle"], "fullWidthﬂ"),
+    flexItem: Control.Boolean("Flex item", false),
+})
