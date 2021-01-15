912ebc57d7e5817294ce83bbdc9c4618f07d1275
Sven Wiegand <sven.wiegand@orgavision.com>
2021-01-15 13:43:27 +0100

    Make InputBase's style configurable

--- code/inputs/InputBase.tsx
+++ code/inputs/InputBase.tsx
@@ -1,35 +1,46 @@
+import { Theme } from "@material-ui/core"
 import MuiInputBase from "@material-ui/core/InputBase"
 import { addPropertyControls } from "framer"
 import * as React from "react"
 import { Control, DefaultControl } from "../common/propertyControl"
 import { useDerivedState } from "../common/state"
-import { withTheme } from "../common/theme"
+import { theme, withTheme } from "../common/theme"
 
-export interface Props extends FormControl {
-    placeholder: string,
-    value: string,
-    multiline: boolean,
-    autoFocus: boolean,
-    type: string,
+const typographyOptions = ["h1", "h2", "h3", "h4", "h5", "h6", "subtitle1", "subtitle2", "body1", "body2", "button", "caption", "overline"] as const
+type Typography = typeof typographyOptions[number]
+export interface Props {
+    value: string
+    typography?: Typography
+    color?: string
+    disabled?: boolean
     onChangeText: (text: string) => void
+    placeholder?: string
+    autoFocus?: boolean
+    multiline?: boolean
+    type?: string
+    fullWidth?: boolean
 }
 export function InputBase(props: Props) {
-    const { value, onChangeText, ...other } = props
+    const { value, typography, color, onChangeText, ...other } = props
     const state = useDerivedState(value, onChangeText)
     state.updateIfDefaultValueChanged(value)
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => state.setValue(event.target.value)
-    return withTheme(<MuiInputBase value={state.value} onChange={handleChange} {...other} />)
+    const inputStyle: React.CSSProperties = { 
+        ...(typography ? theme.typography[typography] as React.CSSProperties : {}),
+        color: props.disabled ? undefined : color
+    }
+    return withTheme(<MuiInputBase value={state.value} inputProps={{style: inputStyle}} onChange={handleChange} {...other} />)
 }
 
 addPropertyControls(InputBase, {
     placeholder: DefaultControl.placeholder,
     value: Control.String("Value", "", "", true),
-    autoFocus: DefaultControl.autoFocus,
     multiline: Control.Boolean("Multiline", false),
     type: Control.Enum("Type", ["text", "number", "password"], "text"),
-    required: DefaultControl.required,
-    error: DefaultControl.error,
+    autoFocus: DefaultControl.autoFocus,
     disabled: DefaultControl.disabled,
+    typography: Control.Enum("Typography", [...typographyOptions], "button"),
+    color: Control.Color("Color", theme.palette.text.primary),
+    onChangeText: Control.EventHandler(),
     fullWidth: DefaultControl.fullWidth,
-    onChangeText: Control.EventHandler() 
 })
